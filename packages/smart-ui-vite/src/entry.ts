3b09ebd5e94eaa1c34106859743bd4356d1940f9
李黎明 <1558449520@qq.com>
2022-11-04 14:12:32 +0800

    feat: finish chapter-1

--- packages/smart-ui-vite/src/entry.ts
+++ packages/smart-ui-vite/src/entry.ts
@@ -1,12 +1,14 @@
 import { App } from "vue";
-import RenderButton from "./button";
+import RenderButton from "./button/render-button";
 import SFCButton from "./sfc-button.vue";
 import TSXButton from "./tsx-button";
+import UButton from "./button";
 
 export { RenderButton, SFCButton, TSXButton };
 
 export default {
   install(app: App): void {
+    app.component(UButton.name, UButton);
     app.component(RenderButton.name, RenderButton);
     app.component(SFCButton.name, SFCButton);
     app.component(TSXButton.name, TSXButton);
--- packages/smart-ui-vite/vite.config.ts
+++ packages/smart-ui-vite/vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 
+import Unocss from "./config/unocss";
+
 const rollupOptions = {
   external: ["vue"],
   output: {
@@ -12,10 +14,11 @@ const rollupOptions = {
 };
 
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [vue(), vueJsx(), Unocss()],
   build: {
     rollupOptions,
     minify: false,
+    cssCodeSplit: true,
     lib: {
       entry: "./src/entry.ts",
       name: "SmartUI",
