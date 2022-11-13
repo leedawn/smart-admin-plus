6b14d36ee5582807c9267e16bec69af28d8d1304
leon <1558449520@qq.com>
2022-11-13 17:31:34 +0800

    fix: pulish npm error

--- packages/smart-ui-vite/publish.sh
+++ packages/smart-ui-vite/publish.sh
@@ -5,6 +5,6 @@ echo "请进行登录操作"
 npm login
 echo "-------publishing------"
 npm publish
-npm config set registry=https://packages.aliyun.com/5eb501ef3fd198000181afca/npm/npm-registry/
+npm config set registry=https://registry.npm.taobao.org
 echo "发布完成"
 exit
