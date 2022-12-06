cf19d6bd444cc80b9b29e5bda79a1eb7f89d5194
leon <1558449520@qq.com>
2022-12-06 22:53:13 +0800

    feat: finish chapter6

--- packages/next-demo/next.config.js
+++ packages/next-demo/next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
+const path = require("path");
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  webpck: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname),
+    };
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
