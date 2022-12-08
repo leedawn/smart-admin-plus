190a169413aad5ac4e496b7512e5873178683820
李黎明 <1558449520@qq.com>
2022-12-08 10:50:45 +0800

    feat(nuxt-demo-plus): 初始化创建 nuxt 项目

---
+++ packages/nuxt-demo-plus/pages/about.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>about</h1>
+    <nuxt-link to="/">home</nuxt-link>
+  </div>
+</template>
+<script>
+export default {
+  head: {
+    title: "about page",
+  },
+};
+</script>
---
+++ packages/nuxt-demo-plus/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>welcome</h1>
+    <nuxt-link to="/about">about</nuxt-link>
+  </div>
+</template>
+<script>
+export default {
+  head: {
+    title: "home page",
+  },
+};
+</script>
