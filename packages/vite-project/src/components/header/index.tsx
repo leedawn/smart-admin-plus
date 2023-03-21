219b18839dd27966c70517d8ee3cc8ab042c9654
李黎明 <1558449520@qq.com>
2023-03-21 18:06:33 +0800

    feat(vite-project): 快速上手和 css 方案

---
+++ packages/vite-project/src/components/header/index.tsx
@@ -0,0 +1,11 @@
+import "./index.scss";
+
+export function Header() {
+  return (
+    <div>
+      <p className="header">header</p>
+      <p className="bg-red-400 text-blue-600">hello</p>
+      <p className="underline text-red-400">hi</p>
+    </div>
+  );
+}
--- packages/vite-project/vite.config.ts
+++ packages/vite-project/vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, normalizePath } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import autoprefixer from "autoprefixer";
+
+const variablePath = normalizePath(path.resolve("./src/variable.scss"));
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    // modules: {
+    //   generateScopedName: "[name]__[local]__[hash:base64:5]",
+    // },
+    // preprocessorOptions: {
+    //   scss: {
+    //     additionalData: `@import "${variablePath}";`,
+    //   },
+    // },
+    // fail
+    postcss: {
+      plugins: [
+        autoprefixer({
+          // 指定目标浏览器
+          overrideBrowserslist: ["Chrome > 40", "ff > 31", "ie 11"],
+        }),
+      ],
+    },
+  },
+});
