7846c5ff15d6cb47912894b6905ff52f49dadaaf
leon <1558449520@qq.com>
2023-08-28 14:21:38 +0800

    feat(utils): add two sorts

---
+++ packages/utils/src/sort/merge.ts
@@ -0,0 +1,25 @@
+export function mergeSort(arr: number[]): number[] {
+  if (arr.length < 2) return arr;
+  const mid = ~~(arr.length / 2);
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid);
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left: number[], right: number[]): number[] {
+  let res = [];
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      res.push(left.shift());
+    } else {
+      res.push(right.shift());
+    }
+  }
+  while (left.length) {
+    res.push(left.shift());
+  }
+  while (right.length) {
+    res.push(right.shift());
+  }
+  return res as number[];
+}
--- packages/utils/test/sort.test.ts
+++ packages/utils/test/sort.test.ts
@@ -1,8 +1,15 @@
 import { quickSort } from "../src/sort/quick";
 import { bubbleSort } from "../src/sort/bubble";
+import { selectionSort } from "../src/sort/selection";
+import { mergeSort } from "../src/sort/merge";
 
-let nums = [34, 2, 46, 6, 8, 35, 23, 29, 1];
-let res = [1, 2, 6, 8, 23, 29, 34, 35, 46];
+let nums: number[] = [];
+let res: number[] = [];
+
+beforeEach(() => {
+  nums = [34, 2, 46, 6, 8, 35, 23, 29, 1];
+  res = [1, 2, 6, 8, 23, 29, 34, 35, 46];
+});
 
 test("bubbleSort", () => {
   expect(bubbleSort(nums)).toEqual(res);
@@ -10,4 +17,12 @@ test("bubbleSort", () => {
 
 test("quickSrot", () => {
   expect(quickSort(nums)).toEqual(res);
+});
+
+it("selectionSort", () => {
+  expect(selectionSort(nums)).toEqual(res);
+});
+
+it("mergeSort", () => {
+  expect(mergeSort(nums)).toEqual(res);
 });
